dc47067e115f1ae8516662a0ba1a84987d751fff
Alekxandr <alkv84@yandex.ru>
2019-07-10 14:00:43 +0300

    Little bit changed urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ from .views import (
 
 app_name = 'blog'
 routes = [
-    ('GET', '/' + app_name, post_list, f'{app_name}:post-list'),
-    ('GET', '/' + app_name + '/{id}', post_detail, f'{app_name}:post-detail'),
+    ('GET', f'/{app_name}/', post_list, f'{app_name}:post-list'),
+    ('GET', f'/{app_name}/' + '{id}', post_detail, f'{app_name}:post-detail'),
 ]
